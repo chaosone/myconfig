6a9f40a624d8439840996e9d8b6127bf96370163
chaosone <solomoon186@gmail.com>
2020-08-16 19:16:46 +0800

    Add files via upload

---
+++ zsh-fuc.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+  function cj ()
+  {
+	cd $(find ~ -type d | fzf)
+  }
+    
+
+  function lisi () {  
+  	        echo 我的名字叫: $1  
+  	}  
+
+  function digital_clock {
+      clear
+      while [ 1 ]
+      do
+          date +'%T'
+          sleep 1
+          clear
+      done
+  }
+  # search in current directory
+  function search {
+      find . -name $1
+  }
+
+  function system_info {
+      echo "### OS information ###"
+      lsb_release -a
+
+      echo
+      echo "### Processor information ###"
+      processor=`grep -wc "processor" /proc/cpuinfo`
+      model=`grep -w "model name" /proc/cpuinfo  | awk -F: '{print $2}'`
+      echo "Processor = $processor"
+      echo "Model     = $model"
+
+      echo
+      echo "### Memory information ###"
+      total=`grep -w "MemTotal" /proc/meminfo | awk '{print $2}'`
+      free=`grep -w "MemFree" /proc/meminfo | awk '{print $2}'`
+      echo "Total memory: $total kB"
+      echo "Free memory : $free kB"
+  }
+
+  function log_msg {
+      echo "[`date '+ %F %T'` ]: $@"
+  }
+##show the size of specific folder
+  function size {
+  	du -sh $1 * | sort -hr
+  }
+
+function fixtp {
+	sudo rmmod i2c_hid && sudo modprobe i2c_hid
+}
